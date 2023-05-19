83dd23182363e977893fd13ce137f8f310d32581
Boof <97455552+hexahigh@users.noreply.github.com>
2023-05-19 12:26:16 +0200

    a

--- index.js
+++ index.js
@@ -5,4 +5,22 @@ function calculateYearsSince(date) {
 }
 
 const myDate = new Date('2009-08-06');
-document.getElementById("age").innerText = calculateYearsSince(myDate);
+document.getElementById("age").innerText = calculateYearsSince(myDate) + " Years old";
+
+
+function countdownTimer() {
+  const now = new Date().getTime();
+  const nextYear = new Date("2023-06-08").getTime();
+  const timeRemaining = nextYear - now;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  const countdownDiv = document.getElementById("countdown");
+  countdownDiv.innerHTML = days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds";
+}
+
+// Call the countdownTimer() function every second using setInterval
+setInterval(countdownTimer, 1000);
