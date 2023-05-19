ead4b05f8435c24370a61b97530396f70ce91ba0
Boof <97455552+hexahigh@users.noreply.github.com>
2023-05-19 11:27:21 +0200

    please work

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+function calculateYearsSince(date) {
+  const diffInMs = Date.now() - date.getTime();
+  const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
+  return Math.floor(diffInYears);
+}
+
 const myDate = new Date('2009-01-01');
 document.getElementById("age") = calculateYearsSince(myDate);
-
-function calculateYearsSince(date) {
-    const diffInMs = Date.now() - date.getTime();
-    const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
-    return Math.floor(diffInYears);
-  }
-  
