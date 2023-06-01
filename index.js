7dd65d45d5852d93527dc2fedc774f926e2f781a
Boof <97455552+hexahigh@users.noreply.github.com>
2023-06-01 08:12:03 +0200

    fixed minute counter

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function calculateYearsSince() {
   const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
   const days = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
   const hours = Math.floor(hoursSince())
+  const minutes = Math.floor(diffInMs / 1000 / 60)
   //const hours = Math.floor((diffInMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((diffInMs % (1000 * 60 * 60)) / (1000 * 60));
+  //const minutes = Math.floor((diffInMs % (1000 * 60 * 60)) / (1000 * 60));
 
   document.getElementById("ageYears").innerText = Math.floor(diffInYears) + " years"
   document.getElementById("ageDays").innerText = days + " days"
