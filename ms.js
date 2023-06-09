890009e58b631372975aa72fe367f9534c1a8ab3
Boof <97455552+hexahigh@users.noreply.github.com>
2023-06-09 11:48:21 +0200

    beep boop

--- ms.js
+++ ms.js
@@ -2,13 +2,7 @@ function calculateYearsSince() {
     const myDate = new Date('2009-06-08');
 
     const diffInMs = Date.now() - myDate.getTime();
-    const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
-    const days = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(hoursSince())
-    const minutes = Math.floor(diffInMs / 1000 / 60)
-    //const hours = Math.floor((diffInMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    //const minutes = Math.floor((diffInMs % (1000 * 60 * 60)) / (1000 * 60));
-
+    
     document.getElementById("number").innerText = diffInMs
 }
 
