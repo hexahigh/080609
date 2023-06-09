8812398b3121cb4f1b3f867da90fce5be7640c4e
Boof <97455552+hexahigh@users.noreply.github.com>
2023-06-09 11:51:33 +0200

    nanosecond cooler

--- ms.js
+++ ms.js
@@ -2,8 +2,9 @@ function calculateYearsSince() {
     const myDate = new Date('2009-06-08');
 
     const diffInMs = Date.now() - myDate.getTime();
-    
-    document.getElementById("number").innerText = diffInMs
+    const diffInNs = diffInMs * 1000000
+
+    document.getElementById("number").innerText = diffInNs
 }
 
 setInterval(calculateYearsSince, 0.1)
