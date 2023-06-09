aae62383e5e663815aeaf3c18cda9b9b31f683bf
Boof <97455552+hexahigh@users.noreply.github.com>
2023-06-09 11:29:41 +0200

    a

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ function countdown() {
   const nextYear = new Date(theDate).getTime();
   const timeRemaining = nextYear - now;
 
+  if (timeRemaining < 0) {
+    year = year + 1
+    countdown()
+  } else {
+    countdown()
+  }
+
   const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
   const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
