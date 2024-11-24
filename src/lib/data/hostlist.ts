d2025444b5366fcf3db94ec071b82cd6fffbddda
Boofdev <hexahigh0@gmail.com>
2024-11-24 19:20:27 +0100

    init new version

---
+++ src/lib/data/hostlist.ts
@@ -0,0 +1,12 @@
+export const hostlist  = [
+    {
+        name: 'Immich',
+        url: 'https://img.080609.xyz',
+        description: 'My private image hosting service.'
+    },
+    {
+        name: 'Zerotier',
+        url: 'https://zt.080609.xyz',
+        description: 'A private Zerotier controller.'
+    }
+];
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { hostlist } from "$lib/data/hostlist";
+</script>
+
+<div class="mt-8 flex justify-center text-center">
+	<div class="prose lg:prose-lg">
+		<h1>Hello, this is my test site.</h1>
+		<p>
+			I selfhost various things on the subdomains of this site, most of which are run on single
+			Raspberry Pi 4.
+		</p>
+		<p>I also host various experiments on this site.</p>
+		<p>Here is a non-exhaustive list of some of the things I host on this site:</p>
+		<ul>
+            {#each hostlist as host}
+                <li class="self-center">
+                    <a href={host.url}>{host.name}</a>
+                    <p>{host.description}</p>
+                </li>
+            {/each}
+		</ul>
+	</div>
+</div>
+
+<style lang="postcss">
+	li p {
+		margin: 0;
+    }
+</style>
