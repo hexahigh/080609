7e5d521b329facfc7baa95c02712df6605701906
Boofdev <hexahigh0@gmail.com>
2024-11-29 22:14:19 +0100

    feat: Added more hosts to hostlist

--- src/lib/data/hostlist.ts
+++ src/lib/data/hostlist.ts
@@ -8,5 +8,20 @@ export const hostlist  = [
         name: 'Zerotier',
         url: 'https://zt.080609.xyz',
         description: 'A private Zerotier controller.'
+    },
+    {
+        name: 'SearxNG',
+        url: 'https://search.080609.xyz',
+        description: 'A public SearxNG instance.'
+    },
+    {
+        name: 'Pocketbase',
+        url: 'https://db.080609.xyz',
+        description: 'A Pocketbase instance, was used by Blålange. Now defunct.'
+    },
+    {
+        name: 'Directus',
+        url: 'https://db.blalange.org',
+        description: 'A Directus instance, used by Blålange.'
     }
 ];
